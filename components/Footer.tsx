5d7b7651ad3911a342727baddc10be40d37b4fd6
Chekkal Youcef <chekkalislam@gmail.com>
2023-02-19 22:06:01 +0100

    added get current year to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -92,7 +92,7 @@ const Footer = () => {
       <p
         className="mt-4 text-center text-sm text-gray-500 dark:text-gray-400 lg:mt-0 lg:text-right"
       >
-        Copyright &copy; 2023. All rights reserved.
+        Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       </p>
     </div>
   </div>
